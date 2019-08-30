194f8a580c63163d8606667c555cdfe1f45b6bea
Greyskin87 <marco.gabba87@gmail.com>
2019-08-30 10:56:04 +0200

    Added Entity Framework and Database

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        //I pass some info to the base constructor; "DefaultConnection" is the connection string defined in Web.config of the WebUI project
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+
+    }
+}
